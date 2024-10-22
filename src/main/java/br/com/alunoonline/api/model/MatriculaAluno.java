79072f9a66148b4d9ecf7c9d91c1dd995eb555bb
Kiellen <kiellenkarolinne123@icloud.com>
2024-10-22 20:40:23 -0300

    adicinei metodo criarMatricula e criei o Enum MatriculaAlunoStatusEnum

---
+++ src/main/java/br/com/alunoonline/api/model/MatriculaAluno.java
@@ -0,0 +1,34 @@
+package br.com.alunoonline.api.model;
+
+import br.com.alunoonline.api.enums.MatriculaAlunoStatusEnum;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class MatriculaAluno implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "aluno_id")
+    private Aluno aluno;
+
+    @ManyToOne
+    @JoinColumn(name = "disciplina_id")
+    private Disciplina disciplina;
+
+    private Double nota1;
+    private Double nota2;
+
+    @Enumerated(EnumType.STRING)
+    private MatriculaAlunoStatusEnum status;
+
+
+}
